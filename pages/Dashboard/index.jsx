3dd704b73dff9d52b33945df93569dcc532a07a5
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-11-10 07:40:02 -0300

    Prepapando o dashboard

---
+++ pages/Dashboard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+      <header>
+        <img src="./GuiDialLogo.png" alt="" />
+        <div className="whiteBox"></div>
+        <div className="whiteBox"></div>
+        <div className="whiteBox"></div>
+      </header>
+      <div className="DedailContainer">
+        <div className="Container">
+          <h2>Torneios disputados</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div className="Container">
+          <h2>Historico de creditos</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export { Dashboard }
--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login/index'
+import { Dashboard } from '../pages/Dashboard'
 
 const App = () => {
   return (
     <>
-      <Login />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   )
