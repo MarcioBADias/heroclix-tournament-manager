2ff5b05bfc8e756b2142728b1ea9b70f157a7d17
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-11-10 07:53:10 -0300

    Esbocando header e home

---
+++ components/Header/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './style'
+
+const Header = () => <Container />
+
+export { Header }
--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login/index'
 import { Dashboard } from '../pages/Dashboard'
+import { Home } from '../pages/Home'
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
