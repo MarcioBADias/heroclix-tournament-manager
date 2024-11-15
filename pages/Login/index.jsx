d1163fd251be55a81cf4d3be608cf54c8e3f290c
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-11-14 22:14:33 -0300

    ✅ Criando logica de validacao do login

--- pages/Login/index.jsx
+++ pages/Login/index.jsx
@@ -8,18 +8,35 @@ import {
   ImgDial,
   Title,
 } from './style'
+import axios from 'axios'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPasswortd] = useState('')
+  const [error, setError] = useState('')
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('httpl://localhost:500/api/login', {
+        email,
+        password,
+      })
+      localStorage.setItem('token', token)
+      window.location.href = '/dashboard'
+    } catch (error) {
+      setError('Credenciais invalidas. Tente novamente')
+    }
+  }
 
   return (
     <Container>
       <Title>Heroclix Tournament Manager</Title>
-      <LoginForm>
+      <LoginForm onSubmit={handleLogin}>
         <ImgDial src="./WkDial_Blue.png" alt="Dial symbol" />
         <ImgLogo src="./GuizLogo.png" alt="Miniatura de Super" />
         <h2>Login</h2>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <Input
           type="email"
           placeholder="Email"
